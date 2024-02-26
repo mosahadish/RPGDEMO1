b9679e481cf9b0ce0b110a66f5fc5cf94e1703e6
mosahadish <93247010+mosahadish@users.noreply.github.com>
2024-02-26 11:00:07 +0200

    removed PositiveZ node and replaced with member variable in Player.cs

--- Classes/GenericComponents/Movement.cs
+++ Classes/GenericComponents/Movement.cs
@@ -29,21 +29,33 @@ namespace Game
         [Export] private Actor actor;
         private Vector3 newVelocity;
 
+        private float lerpWeight = 0f;
+
         public override void _EnterTree()
         {
             base._EnterTree();
             SetSpeedValues();
         }
 
+        public override void _Ready()
+        {
+            base._Ready();
+            lerpWeight = 0f;
+        }
+
         public void HandleMovement(Vector3 direction, double delta)
         {
-            direction = direction.Normalized();
+            //direction = direction.Normalized();
             
             newVelocity = actor.Velocity;
             newVelocity.X = direction.X * (CurrentSpeed * (float)delta);
             newVelocity.Z = direction.Z * (CurrentSpeed * (float)delta);
 
-            actor.Velocity = actor.Velocity.Slerp(newVelocity, 0.1f);
+            // lerpWeight += (float)delta;
+            // if (lerpWeight >= 0.25f) lerpWeight = 0.25f;
+
+            actor.Velocity = actor.Velocity.Slerp(newVelocity, 0.15f); // For some reason 0.2f and above causes exceptions
+            
             actor.MoveAndSlide();
         }
 
